dd18e8d0c8056522a5598014dd642aabe5016983
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2023-12-04 18:44:57 +0530

    Create specom23.php

---
+++ sysgall/specom23.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SPECOM'23 Visit</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Jost-Light', sans-serif;
+            }
+            h1 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h2 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h3 {
+                color: #000c65;
+                text-align: center;
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #22be9a;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://syspin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/syspin-logo.png" 
+            alt="SYSPIN">
+        </a>
+</div>
+    
+        <h1>SYSPIN </h1>
+        <h1>Text-to-speech synthesizer in nine Indian languages</h1>
+        <h2>SPECOM'23 Visit</h2>
+<div class="container" >
+    <div class="row">
+        <div class="album-card-br">
+            <br><h3 >SPECOM'23 Visit</h3>
+            
+          //=============================Add Google Embedded Code =========================================//
+        </div>   
+    </div>        
+</div>
+</body>
+</html> 
