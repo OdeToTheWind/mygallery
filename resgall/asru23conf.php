e25b462fb37e7ad46635948a335c3cb2591b7c58
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2023-12-22 15:40:49 +0530

    Create asru23conf.php

---
+++ resgall/asru23conf.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ASRU'23 Conferenece</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Roboto', sans-serif;
+            }
+            h1 {
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h2{
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h3   {
+                color: #352010f7;
+                text-align:center;
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #908dff;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://respin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/respin-logo.png" 
+            alt="RESPIN">
+        </a>
+</div>
+    
+        <h1>RESPIN</h1>
+        <h1>Speech Recognition in Agriculture and Finance for the Poor in India</h1>
+        <h2>ASRU'23 Conference</h2>
+<div class="container" >
+    <div class="row">
+    <div class="album-card-br">
+            <br><h3 >Visit to CSTS</h3>
+            <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+            <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+                data-link="https://photos.app.goo.gl/nKaCxMQbYWpLLzSaA"
+                data-title="Visit to CSTS"
+                data-description="2 new items added to shared album">
+                <object data="https://lh3.googleusercontent.com/pw/AJFCJaXnUTnHo81ZXH0ZEYytQD9n3tpghyDQC8IqbuOl4Wx41SbFrHyoW4FRzaggu0nuDyjKdLHvmpQcw78uX8Ifa22jtHJWErSckVdoLbKIxtAQUkocVcU=w1920-h1080"></object>
+                <object data="https://lh3.googleusercontent.com/pw/AJFCJaUgPg1NAUcfpBivAdEgefqlF3a0B__fxm7JFioYpYPpU57HknowUOjhQtFowONG70zMaOw7dIWf83-iOLPx5Wz5hDLX8Ok7nLE1_ixaubcs4EFsylA=w1920-h1080"></object>
+            </div>
+        </div>
+    </div>        
+</div>
+</body>
+</html> 
