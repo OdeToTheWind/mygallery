67eb572852e5161641968c77a365977418314ac9
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2023-12-22 15:48:59 +0530

    Update specom23.php

--- sysgall/specom23.php
+++ sysgall/specom23.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>SPECOM'23 Visit</title>
+        <title>SPECOM'23 Conference</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
