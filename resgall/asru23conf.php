31975f3777e78ed9f42125bed432cea5b010d3a9
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2023-12-28 13:39:48 +0530

    Update asru23conf.php

--- resgall/asru23conf.php
+++ resgall/asru23conf.php
@@ -116,6 +116,28 @@
     <div class="row">
     <div class="album-card-br">
             <br><h3 >Visit to CSTS</h3>
+        <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+        <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+          data-link="https://photos.app.goo.gl/Cz8ZhLhnVDNqZmxX6"
+          data-title="ASRU&#39; 23 Conference"
+          data-description="15 new items · Album by Speech Recognition India">
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87rdy76CswHutiQs_I6khxwv_8Q9S4m2u-eK6dSZFZeBmNRGS2iWiG9nxIg-91S7xghP4DAjWDHeKEYr3dFjRg7DpQeuL8O7RDxVeuBaNoUMzPDU9I=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87e7DdhC7_4Fxt8Cb3xNSeKEX1LTbg5p1zfX_CJEfMHziEQPxUKEyoL9mypppTlGMhAqKKAFOUuIl0DEMy60UaN7AFLaXPOo7Y29SxqBBg__mPmJag=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87005R981-1w2e7vsbuR6-gNephs5LwCcTpRjxMSsiOeIQ6B7K0ZoPFem9IBpjrk_d1AoiNjJbn5-GRcF3vD7Nkl8RuOJuiz6fhu9PYEQSjNuLQjNk=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87jE7D9lY6Jm9Sg3oag9S2EyQPYqdovJq-h-criNGwApv-HSTXP4lnnTcMKo_fWDo37qE0JPM60N45bhJoQOBFMdBPfhoOWchL6YC2qp2BSE5JXT-Q=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV84k7QMd1sMa6rCvTblo4UtoDARL2ny2Aqvktw3-J6sUZQMgcqaNpIcS_0v_cAKDproGSOQFPjPKSqjVdOSCzZEwVfX-W2P2W4jQhoXbkVlVRQ25uGI=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV8793vTDNgw3U30W38HIKwr4O3DHaGd_gS1K9KPh4f4PnVJz067LNfanPSyykH2h9tOJwgbg-E1maJKsYsWKBbjVVpGf18cfOdpw9tKBmQ7K3Qil42o=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV85tQYBu8kSJzKaNwROXZuB28ZFHWO0w_tkS0YPRIF0B9FNMvYw5ePRU31cyA_OOCqECZgJC8_vs07RwkETmtAEO1-mygYFOMl4TlxBmyfLN7eU-Cr0=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV86ZeVe4T0Au4xKna9E1xqba5Bkkrs6hJo2nLVJ70HGBmWX6TdT0GCJmfbYX8LVaiW4fK9zOJEV9dD1mVAJVU6WRsKm836e5qYdcXSFY03t61pqBrbY=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87j5B32oZBoyYuX533WNDRPhnAzFvvqV-qsAsjhPR08rGcxf9gPDLc2KlZhO_2F8N_KMhRBxBje7HuylSZpTfA75r278brzC33_fie-Ww-lwj2AqXU=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV85_IR_IRrHRZOKE--5vnJqFIsSWu2aCU4AY4MQq6xPOAEQVH9ZxZcZTuVJ4l0z3nLMZc45klHDvsRAVzWs-CDFvfNqSwYsvB1nPzuHtY42tRyXATY8=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV86aWdhd7rnUo6GzysWurhOn5ExfBm68Le5EkoRAFARQBX6RXzDAv-J7M3HH-iawjpHauBG-tyXEtLPEXcXZbPE2Nqs0e2-95Ryxo8cxvv3NneAQ3-0=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87FdrkXRBb67aDJkN6ZFs6va-vPpC4yYVK0nq_cAt9UfDprjT2bFgxcvhb-guSRxuEymvLhtsDGdFRwLG3cWPeE9XPEbgRv5HT0txiiVsEi8OCfTow=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV87FEQroBhHqAQ-iJRvTHIYonUeJ2ORCsm6OhlZf3kduoFrRVoxZ2phM1fe5AnGB_9ISI8jKmqtSEEQ2ss-oq3r0J6mfxPfVhGRNzJSGUviQfx_xvI4=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV84LdD7lvKm1Jehln05Jfjck9gDdAE6aj7C_wU-tUPF4QLTRteK3S7IEGL4rpM0cCKYRcUfZdF0tu3f7smzZbNFkztJXDNAwpT14EzBWFAES41BV11Q=w1920-h1080"></object>
+              <object data="https://lh3.googleusercontent.com/pw/ABLVV86tFJrT0kLDoaIAiFblPqDi83lOMNdadhGAS44odq6OR1tb-qg0r-5Aw5tqiqUuuKDZ8WxGgeU5bpAbXCn6bjAKKsM1eFg_fhYMhno58HEQiytFJYY=w1920-h1080"></object>
+        </div>
+
             
     </div>
     </div>        
