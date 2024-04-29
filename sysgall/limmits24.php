676757d55ccd88adbee37e9224d185344a67c6d7
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2024-04-29 18:50:53 +0530

    Create limmits24.php

---
+++ sysgall/limmits24.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LIMMITS’24</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Jost-Light', sans-serif;
+            }
+            h1 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h2 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h3 {
+                color: #000c65;
+                text-align: center;
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #22be9a;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://syspin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/syspin-logo.png" 
+            alt="SYSPIN">
+        </a>
+</div>
+    
+        <h1>SYSPIN </h1>
+        <h1>Text-to-speech synthesizer in nine Indian languages</h1>
+        <h2>LIMMITS’24 - LIghtweight, Multi-Speaker, Multi-Lingual Indic TTS</h2>
+<div class="container" >
+    <div class="row">
+        <div class="album-card-br">
+            <br><h3 >LIMMITS’24</h3>
+                <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+                <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+                  data-link="https://photos.app.goo.gl/T9M68kj3vhnVjQXE9"
+                  data-title="Limmits&#39;24"
+                  data-description="19 new items · Album by Speech Recognition India">
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOow8XFP3c9i4DJcQ3RU4Udk-eQHdcsKOGNRxGA9YuAmdxEzd_xUG7n794a5qyf2TnP3Ug1qQns9Z5jBwQoZO1eh4moASgwQkZZ8p9H-UO32PSHlJg=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczO_TmzXkvUhrZ4rY_h9SFcYos5jQUI2C5ZhXWAtmc8SXgd_7k4-ve7HGCHxJ7uecNSShTPmUJDdUX-2fS9iXpG4v-PKrkqjcpCV3J2pxTWn2fJU52A=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOTPu0ma9VbFsXses29WEUMRENGh-U4tfm6zkJtRXWbZB_uINeLVRuocfWlcixPfBMF9JxeVgjOEs0xd8eUeTDad3eoHeGQqsA3Rdvs7QzHrKXifN8=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczO6jULzByrhh_ISn1tL7_LTFNgrZg0ZmmbJ1USKu2PtcwNm_Gc-3oQ3Mu6uEOY_uyAx4J-mLSZMqZz4scDxprqQRS5sfG8TszR25_mtOIuVkQDilxs=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczPYLP1pl0QpNJcDYwnUzusRc9uj0uYGKqLhvwGg3xNTTSvQZSQnQ6-W8Uem-8M9WEVHWu1ELgNmW_euvCe_zFj_hv4bTvU9hyFnXLovYdB0nHbKRVc=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOCOhCbMRe6YxYBDopfu17DYfvXirhKZcZLlQIVTX175AtWr1wTKDdoChAwkxa3xQfDi_LkidkD-NQycICjLpMjLdCRz3pFqTTzb5CS4UImP7kw66c=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOFJO4CrIOeb5ZpIPZ4M4cXrk9FVPiSUt_86WZs0O2DpNeQTm9LwOSMQQtTc6fvdJe-sSXMgGQv-IC0r5uvDOySQ4Z1WvZGktRLCutnDH_F6FuuoYw=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOX1sOqHLDsk0sNGKN1Mx1HFN7pZVvFF7gT5w9SWE3FRaS0U2wDyHUGfttLqGD3gC_nxFPGMVgbJ29CTs7Glt06DduUqarxfBfu0qymwrfaIpFRvkM=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczMuTdZJjZ5VFNIjGo7Qk5DLE6PqnuxzJiq4u_XV3Ov7eP5UBAzpvissi4MRKiUW1MKTVQpNcxEfr1_0VvTuKYK_Zsc4CPNR1JRhJZYB1XRkYvN7Niw=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczPttfGgbREqdmy3S1isBcVKpTgrPpu1urd-Uf-8CDxKdTorYtTboNIRnrIqeaFublLgF5cP9TeSQ2wdTtiCu9YkREcKPtblBwKJ00WnoMYZtY0LAFo=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczMuIWTUyRaHlNCJS4G8bnPZAG80dSbhA5Np-if4G1w8eGoNRpHu5jPKsCM4-LVpzTehCbdexkAVWbBZE48IqMDz_ouz_d9BCNSS1r7UUlPlXhiJ3a0=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczPY-fWbgTHYkiGJBt-tJX8MJFfK6tadGG4piUFAKst65uqomccnV8zL7b4opH4_78R4K85kRdY9mW9bdrwVeEV5DK3eRqKqDZb15k2L1zSI6qaZYkU=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOUolMgsRyg59zRY_8aWaF9nS--tXaYiMa-aGLfmH77hF7IGbYXEy_M41DtaTcPqOTK6LfYR0gJAeyqq0h78TsUOU4_gturqUQPbaTMiy_BYobUW5g=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczMleK3WSwu2er9HvZkI1_A7c-18da7pAPuAMbP9_oOsp_9boOOBMWYDVG4_HofY4IfIUQQzNMppZaHlPH2qG7TE-LRmNJIyqLRssaxNRFOtkCfUIsc=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOBeVM_jP15o1-ds945vAYRyYaqCKd2itAgFTmrmD9JkMpWLnU7w2Z_cYwrK67niUKXp0wktTvzBwIthXfW5V3yephprBfBiqjB35hBCcEOaz6UBYM=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczMlSZRIoVJ_9XlvN9LTpziwiQD4sovJ1aWuxP9U1JI20BG1U3kPbA9YArpXs9mE9sI7-e0dDyyT6m-apAv5y7Ed3yP5mtGZf-pKU4pSCLMxn-Udr58=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOC9cUoM6UInM1_HcX7I61CkunuU_phl4_A_3J70VGNPnZ4Y6w42ugejRqggNPxFH_orA-Sv6ZfJSQB8XbQ8AZ00Wc8fy21OBEyqfgk3Mctr97su0Q=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOV_E6fvoR8gDduE1eWn677Q_8JG5CrWNw5bnHYbPLmnl7DJsNOfXbC1zaGSki_3tqf9CnZdnO2Pd91FzF0DWWHviEK51YKzlpFJ76Bsuef48ltsuY=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczNTmab0KKC5aj-xnQUYsBis-fkHsl5-genXx7ZDVidWi-5ZS6EI8AypbBn3pIrSvxshI8hyuOOVuq9GP12TSv4VP92EUFH_TeXXXVANd17hZX6ZArE=w1920-h1080"></object>
+              </div>
+
+        </div>   
+    </div>        
+</div>
+</body>
+</html> 
