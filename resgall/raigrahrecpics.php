4c6ad6f469cb5b39905a7c3e35b9c0ee69adb9cd
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2024-07-11 14:09:36 +0530

    Created raigrahrecpics.php

---
+++ resgall/raigrahrecpics.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Raigrah Racording Photos</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Roboto', sans-serif;
+            }
+            h1 {
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h2{
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h3   {
+                color: #352010f7;
+                text-align:center;
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #908dff;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://respin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/respin-logo.png" 
+            alt="RESPIN">
+        </a>
+</div>
+    
+        <h1>RESPIN</h1>
+        <h1>Speech Recognition in Agriculture and Finance for the Poor in India</h1>
+        <h2>Raigrah Racording Photos</h2>
+<div class="container" >
+    <div class="row">
+    <div class="album-card-br">
+            <br><h3 >Raigrah Racording Photos</h3>
+        
+        <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+<div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+  data-link="https://photos.app.goo.gl/T4KCpU5Pv91WU7D8A"
+  data-title="Raigarh Recording Photos"
+  data-description="27 new items · Album by Speech Recognition India">
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczOUivNx9jtWYhY7wAWCSes3I7w8SU8CN6E_NQhh4xpOEfP_q_-jr0et6FuP_ohh2i6zGm3JqnoYjyp3FzYIYU7CrzTp8rHc7kPCYly7hNWhZPBw3ro=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczMYIDuOlmfbo23ZYuikP4SG7z61wBgIxQY3Or7qXQXwyKgMSyBvEWwM2EZq9A_BqwRPwJY9qGbIlYtJ5jHWB_MhIM1pt08ZzwKx1QH9B6QL9CN9LhA=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPzlPaHgHVcd1ZAPaZbJ2vXMPFgSESEeXU9HLDbccZngZiity0Lgwmz5qq590yi1-BqWPil7PJQvi6xcKB5V7lZPGSL8kBBcW2pCrj-NMJ8s8qzfsk=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczO5-wxrNIoUja3E_kCUuQD-NQBlvMy_NCtLXldO1kP5VP2tjgCPsr0PjhXezBRJYy2lH2O93XSHJQSxValNZDlO6AqaAFWy5q11JzNfLnMK7T-GJJ4=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczMSeDmzzj5y96ddbr41BKp7psalXSyJkf3l7dFr23ntYo0i0G_XQc46C9hdy4Hie8fL9ScpzYMCrPzVRc25O-EFiIWSTF3P4WILO-Z03_v4BlZMA9k=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPzrhO3heL31ugBClaK2xhqNe1ymKgLG8kCCSN302VdAheT2DdxINWH0PDXW6ueJqKZckrEddmqRdHUNTiRsV9iRyTYwoN0tojNktwAqCVGOBOFdTI=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczMUKx4TncpDgGuX1fEsjyUpygiPY-0Mw6RsSgGDWZid7rx4Vz3GgNn-2tmJGiFKxdA5jlJGFvlitFlPJLTnLNVI_wMAQcwpK2jo8aHO-n3TQ0RDy4s=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczNJq68LLRN-_2Tpw-QvKqEaf9lGDL21xcpM1UjbpzP4rrO6aaoM0xmZe35KhIOFh7d45Mi81dpBCKl7SlhhnUsKPsYl4oVZZF6jnkItjEpYRjVz4Q8=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczOFLS9-mp8s7V3PQHzkM7EcMaVIE-MjK390Elv0CiJ7prb5QSI4ImnBebd2FYEodvquB6-qb0boIiNydkrd8dTpdhJsMa5mBuKJJvQd6lqitkLBxSE=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczO9QyUke4hpxsbM-VGAye1QnaK1PKu3oLTEtB5UcNCZL3WzGlWncEsQGa6PQ7iVWZmqUh6bEJNskNC9HpFLsMWO-6UKhkl-KY6_OFss5-lQD2Qtais=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPmD_eg4tH5KILtphVnDA_R5zzYRKssA1BoiXqBSKxr2PK-YtUuwdlztKlq2IBGDe_nz_e_w1PoC0Bl8aeGgP1_jYUs-fE8uUVcmf4ij2I3rbIJnlE=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPIIRXDxiRi2UyG-HplabjQlHIdu8EJWROSdFlzJl-qPW_OyHQXpQvcQLF-1WEPJMMCq3tZimkiiy7LDZPAu5jj-Hl-GIhsYBLG1ywR7Zjf2WiAnbE=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczMo7uM-osycrvUk9GaISkI1rE1obamPximsHURsBE4wjKNrE3Ug-eBcpUA4ZGhSRUY0uFYxMEijDnGUFVnZZxKQvgWiMsuCgRcsf51Vp_1zUHgvqgk=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczOZm_vd8cigxmInyj6xUMi5cyd9-OI0d5VBVAJRamnOuZi4ETgAHL2-JADH2T3rgKNiy8wtwKHK2u5FofKCYB5TnQmT_S4jSXnIkTH8q9d6CG_0TOM=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczN8VTVvIvEyAB5aXhSj7NSRKLSE-hv07TlgCnx9iQA6SIzklmMpPkwGNSMfijcFd05MBwkz8pUiuoX-fQve__la4ny8FEmOqbBUd6WpHFU6bENe_mI=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczObHSHP9tNu_cBboItbP75xdoPS3dbV8qyZwPBnaGWr3fUkSH1tkZKWVXRtaW5kADxYFl67mbO1H7RmBozm3RL_LpjoeoPZR7CFGA5lMFLVEvVZ_Bk=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczM9e_uaM-IbDN0m9B3lYOjtmM7X9RWxSMqJKA0HNPUvMoe1uUSvmK1n6zFBrQWwR17He91A3sD5_fyPOJw6clzJAGd-x1xGrQF94SGhydLX32C2fcA=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPV6WqignkFR1ctEck78nPP8TxoakgqQ0DYzXKQ6tLkWNB-D7o36cO1Z36fqYJgBYivZTwy61mpKcM6H9yv6DTesR9YP8XhMp1TAc3HtErpZ0lB-9g=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPvkzSpKB-oyuCYJK_HRWrSSI2LgRGnsjKVECLTQ8IQi-k2pbESo93zqkrPgxH6h8KCO2k7P_XJ7mJ9MT0EhO2mZyDL6iUvRVJPLmJCbA-FO1AMgzQ=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczP6bshgV7eo2wy1Qg3ImgJwSD8HO396_q3TBtQMY8lhX1AW2bnNBdzZgOO_p9xso3yRoexMn7K7g-G_1Y4BlyBzIlqzRT5bbSVGr9DeJ9lj802Gfm4=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPxJhr4KN7ylJx0yywt4ulNI_K9daTUNp7SPbXA5qSiS3yzImbsAbjje73TRehmZ1tpUbs6N1xHdyrGv_W8wE3HvZHEpEWGKW4FA89P9wlg6h5Nmtc=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczODRCtLKi5skn_raXx6rQrwugtonphovRic10CHfSRcnHM6zAcptU0X0VAmVx9M5nQitYmswlqVsJ6lx6vwiJhMiWboUL9DAUYK3g_Sx-2Q53kcQoU=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczO_1NjONGyWCZjIV6fDIx6adtAC0Ui7T3lLC8sSRcdPQhpCDCPmH3yAff2I3jPnDqcer8hPMqDOGbZYwK6VB_QgY6NMw6gkxEEf7PxM6Bi6sEVWfWU=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczMrIlkTPqDckiZgDNbJBJypgo9hLe_xHMNx7ra5Y5HjvqOTMomOy8NnpSYeWkiC817YxQscesDkMoA6YwAUWZrfLppAStVKJ5VG-dcKNsiZ1yfM7N4=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczPUzYgY6N3I9SwvWS3y8D7tV7NEzLnOj0HWPslzy8oEqAVvP7bPrHsPI_zuBBCWHSqQOYHFC_2_hthM7YeXpydHIUlkgaew9dMq6mJOM-_2p6vtPqg=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczOkMTwH_rNLnUDQiGVOgKrrZGBmlxda6EYdWW-LKrLUscJkeGIjZk4-GpNZhjjVEyylDEWtvhIaZN2HQm2YqTqDZ6ZzYFth0sMXeIhQV3m8FHsdfBo=w1920-h1080"></object>
+  <object data="https://lh3.googleusercontent.com/pw/AP1GczN4ZZUutW2QEJJvY816e1BucPIqJrgdmxQ403Ejnuy3wLrX9y6nkx_6Mim-90fcAGcqk5d0pq0XMB4DsJ_8P9nVHOMYKkzQWKP02RazRAaU6mb8Xpo=w1920-h1080"></object>
+</div>
+
+
+
+            
+    </div>
+    </div>        
+</div>
+</body>
+</html> 
