c77f3b9a4f7ab214377cbd261a07f9a0a701593c
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2025-04-16 18:27:01 +0530

    Create bmgf23meeting.php

---
+++ resgall/bmgf23meeting.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BMGF's Visit in 2023</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Roboto', sans-serif;
+            }
+            h1 {
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h2{
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align:center;
+            }
+            h3   {
+                color: #352010f7;
+                text-align:center;
+                font-family: NunitoSans-Light;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #908dff;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://respin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/respin-logo.png" 
+            alt="RESPIN">
+        </a>
+</div>
+    
+        <h1>RESPIN</h1>
+        <h1>Speech Recognition in Agriculture and Finance for the Poor in India</h1>
+        <h2>BMGF's Visit in 2023</h2>
+<div class="container" >
+    <div class="row">
+    <div class="album-card-br">
+            <br><h3 >BMGF's Visit in 2023</h3>
+        
+        <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+        <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+          data-link="https://photos.app.goo.gl/FvFTST892yX2LTN47"
+          data-title="BMGF&#39;s Meeting &#39;23"
+          data-description="10 new items · Album by Speech Recognition India">
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczN7O1xE63Qa9llsz9H03cSvUv7dOgpz9pzlCSt19Yp3hhw0kOzBRFDFutGBI2nQSmS31rdvPYxtkBATcMvZaeDa9ALXxHMjqwad5XQJMnD14CjCayaI=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczNZbDTYboWUBMKywhTDG0muSDWUAZ9xHaWj53gxW463X1vJBUH93drbhN1xTYRPGDuNJBuD8v1gO6d3i_Jx_5Q53BcD5C8aCv3RteIJfTv08lIp7fzl=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczPOKd7d61EZShdAqR7LyETxyXgmhHvAgJPPddfASi5bExGCmmSFpep-X7rsCJGafI_F4Juph7QSgVayE5PZrJuC-bwH3jdIBMIOpfXVCPa_-IkHfmVA=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczMUUH3tDfjuihw4gHTJ1tAoVUysxbAu9lKJYulvIcc38tWmbFeP6Fl9nfyzeX3DrEp1UVDi3E6Bb1G1E-SrkMaCTmfR8NUjyPwS2CijsamZWSe8Iq5z=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczMDDh8K7vEWcZCg16nVd7i56M6A-Nx7KzKYU4SA0yw96WkTumRToXo5sdizGzQQGR1nbGbwYrhcJsie4m4vm2o9eKGkVDt57T4yt7jRRqg4eBUrb-oS=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczNhHa0mmBJ2Ur7y_5rvkICxo0GOPhEtB5yNNVwYdVsGSXvmN7hBjfF_dWC-iBI97ubOGym3BoW1f8YLKAHmhb9gOzt5ZJi4zGQQpSrrpIEbAnKeA31d=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczO95iyM9c6ZZiyUq-onXmBTDsttJhD-VbSFm50UeTdakRojKWmJVmd4z1B92JOk8sCOQWIXjulxkn12BELkC896aUNyIYgF-GRGArGTr50ipPdTzPuO=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczOt8UCy71nbpWYCXycQOQiEhD3IR7FYlPlnhTUQsPQ87FH2jt9tS2FPNgTIwjQQ4pvGuMximfQBE45GLK_x9STWIGwDpjEyFScZGFKhmA8wQOyWKm4=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczNyitVqoyh0UeXZXXV9zexxEAdqDUCkKRmXPIH_3EWhsfllQ1gPtcxXWTA2I1jXkRBOGbzQweubXD2JjS3eFhcXWtI5sUFWXO1QPY5ESQmSn8TOXbY=w1920-h1080"></object>
+          <object data="https://lh3.googleusercontent.com/pw/AP1GczM0qusSEiJ0gbKHoPjjjcN7vx5zBb3LyPuTc-AsfoW82e7TAjNY96NhLcsp9pkUf6eFD67RH-HY4nx0BLnyl5Zl94tjdiKgRbsRHD6kot5HykbhVwU=w1920-h1080"></object>
+        </div>
+
+
+
+            
+    </div>
+    </div>        
+</div>
+</body>
+</html> 
