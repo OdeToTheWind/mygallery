4c1ad476c62e2cacb691f8a0803dda415b4fbe27
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2025-04-17 18:26:20 +0530

    Create limmits25.php

---
+++ sysgall/limmits25.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LIMMITS'25</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Jost-Light', sans-serif;
+            }
+            h1 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h2 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h3 {
+                color: #000c65;
+                text-align: center;
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #22be9a;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://syspin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/syspin-logo.png" 
+            alt="SYSPIN">
+        </a>
+</div>
+    
+        <h1>SYSPIN </h1>
+        <h1>Text-to-speech synthesizer in nine Indian languages</h1>
+        <h2>LIMMITS'25 - LIghtweight, Multi-Speaker, Multi-Lingual Indic TTS</h2>
+<div class="container" >
+    <div class="row">
+        <div class="album-card-br">
+            <br><h3 >LIMMITS'25</h3>
+                <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+                <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+                  data-link="https://photos.app.goo.gl/RguDa3h3SUxMB7Jp8"
+                  data-title="LIMMITS&#39;25"
+                  data-description="7 new items · Album by Speech Recognition India">
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOrb1zfmztgl4Z4Dezxl0gCmljg-e9qUXE73XVnPYBG6zLrpgmeha_FcuY7o1QWj_IaY5NC5hGTkUMBzUJ1T9pu41NnCEshWwb6R8ZTcHjBhzyJxXw=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOd-_7CMB5hMnvoTRSk_OL0RRQOcDLH5VXYCU04Mug7Pd7OQMt5IdEBTAfc4zahRfNpfcEshkey0We7iTjnCkVhOPKRXKFncSN_fJrOzicxyKC8Mu8=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczN5nBlnepzMdtRhrTbogg141cqKAQTb3RtSO6BW-rCqYLo76oY1LQynIvlg9Yl8EBonk35Y5WMICv0M7ZGiFu9eyWDNH1hU7c--ZTQ9Zh11nRY45wI=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczNNGUSkXePjrFkAen-oWYxOjDR1S-I0T65I896ezq0IIQcAI1s0PMA6TWtq8e4t16pVkb_8Cl6btSMs6F5oj--rLt060Qfns-QRr-mGhrBdrQICSSM=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczPCJysgXhVQFNA6dWyEnLY_u9QlRo1afEnUdHHaX6YPO36jiDvu2c-AmW14w8yIMnvtpaYb0sdY-5kweMq1DkojwgIsUkfBxNdnxNn6DhSG3MVCE1c=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczNrt5sejLYB9SOI58Hjn99f5rkgB-jcmDo9QiEfIKuzP0V_bvp2Qq3kkmnKW9Xf6ZIVvg8srRlMknLo0XWbpc6uW17Qr0Xcb79T4BqPkTO1OEWmyn0=w1920-h1080"></object>
+                  <object data="https://lh3.googleusercontent.com/pw/AP1GczOkmZcrtdZ5ZVsZEVC-tbr6PRiaa_UxMczKYkPIZkiSMQsBi-T1VOtNwkIZBX7wPdJywMy-JTEPaLEGVq3YwzF8J4_rUPa-a4O9QqzBnOZn65lxTow=w1920-h1080"></object>
+                </div>
+
+
+
+        </div>   
+    </div>        
+</div>
+</body>
+</html> 
