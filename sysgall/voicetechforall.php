064ec3218fba178b5ccdb490ce55c16a2be3c259
Bhargavi Badal <87317399+OdeToTheWind@users.noreply.github.com>
2026-02-04 20:35:10 +0530

    voice tech for all gallery up

---
+++ sysgall/voicetechforall.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Voice Tech For All -- Hackathon 2026</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&family=Roboto&display=swap" rel="stylesheet"> 
+        <style>
+            body {
+                font-family: 'Jost-Light', sans-serif;
+            }
+            h1 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h2 {
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                font-stretch: normal;
+                letter-spacing: normal;
+                text-align: center;
+                color: #000c65;
+            }
+            h3 {
+                color: #000c65;
+                text-align: center;
+                font-family: 'Jost-Light', sans-serif;
+                font-weight: 500;
+                font-style: normal;
+            }
+            .album-card-br {
+                width: 70%;
+                margin: 1.5rem;
+                border-radius: 5px;
+                box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+                color: #000;
+                border: .35rem solid #22be9a;
+            }
+
+            /* .album-card-br:nth-child(2n+1) {
+                border-left: .35rem solid #22be9a;
+            }
+
+            .album-card-br:nth-child(2n) {
+                border-top: .35rem solid #908dff;
+            } */
+
+            /* .album-card-br:hover {
+                transform: scale(1.05);
+                transition: transform 0.3s ease;
+            } */
+
+            .row {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-left: 4.5rem;
+                padding-right: 4.5rem;
+            }
+            @media (min-width: 992px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 991px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            @media (max-width: 767px) {
+                .row {
+                    justify-content: center;
+                }
+
+                .album-card-br {
+                    width: 70%;
+                }
+            }
+            .container {
+                padding: 4.5rem;
+            }
+            .logo {
+                cursor: pointer;
+                position: absolute;
+                left: .75rem;
+                top: .5rem;
+            }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </head>
+    <body>
+<div>
+    <a href="https://syspin.iisc.ac.in/">
+        <img class="logo" 
+            src="../images/syspin-logo.png" 
+            alt="SYSPIN">
+        </a>
+</div>
+    
+        <h1>SYSPIN </h1>
+        <h1>Text-to-speech synthesizer in nine Indian languages</h1>
+        <h2>Voice Tech For All -- Hackathon 2026</h2>
+<div class="container" >
+    <div class="row">
+        <div class="album-card-br">
+            <br><h3 >Voice Tech For All -- Hackathon 2026</h3>
+                <script src="https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js" async></script>
+                  <div class="pa-gallery-player-widget" style="width:100%; height:580px; display:none;"
+                    data-link="https://photos.app.goo.gl/HMraXtbUcNmhugZN9"
+                    data-title="Voice Tech For All -- Hackathon 2026 · Jan 28 – 29 📸"
+  
+                    data-description="Shared album · Tap to view!">
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMXygr41NBL3Ldi1RZRmKJT2Uf5Zb6d5ZXccgIkrKxEbTf-NvLBqGSssxpOqg22UCvhP2l-hVUh0k7weTxXQB-qDzKf5EzGLtY0_D70LcovYC-2GIY=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMeYJryr9rzHWij5ERgIObfPvjvEnAOp8n9MPIy5Co7hKJMoetknqNHjQzgvaL0j90OQWFlCa-iPjquZ8EEABEmAfojsf6hH8RvQuKZtA1_iky5860=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNoZ3JCvjDZnWnHcpawUyVS8fRVpyXPv-U-Q9-xHyDTOETp06czFAAxYb1xsSOvN-dXJNnBroc09d5tkfGUYJU50kvc7_1t6dtD7PhfCQH80ZqxaK8=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczN-4mvsXXH5lwe74pzentkCQxiTU0s4yS9-clnxDz8DaTK8Esxt7AyvgCh9ZMQOaziPJZuxgz2mkn19Vq3gHs4K6TOKmFU1PaeBY1iJpq3IY-vBSFM=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMKGRXn-ACc8uxK9W4VDwnT-kyeZy3WJsKMNEGhtqQfhjB7pA5FPFGe_xfFNgucpOIUyQ6UYUSOdiFcY5iVtrKrUPOdPgvEVLEuGzvvenw--QtKVJM=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMeA3saoWRsBXJNfpKQjT7VbvZeu6XuZQUoIckNZQlidPLIR5vLnjZqreIqsfcSX8XYsc-PcTOR2rMaAlVkwEDXU17W5LYOmp379gvdpt9AHdg0RlM=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMk0q_TJ8zMUlKAwnHdEolDuodze84nyqKbdHdNzHfQ_gBHqmPId8UzjgjEqx3SNAMlutsSknXj6pZgHQJaFd3h9PgeCGZWTwmUx2USJeTClnL9O4s=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczM4lJ9QEdmbWT3dnRoDNxRNBsc4ISdRRaH0By-SqjxZkdDCZC4MbR2zgSJga_Tkqr865jJcuwsCOaaOOIPdmvS7bGn6YmtUl9vWLip_M8sDA0JbsSo=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczOFJ0DkMS_9GKpQdiGvEjg8U37k8b27MTRUbWcN732DCsnIZObldy1Uk7_H2Y1_RDIOt3i6odT81HZaw5x6JF5w3wMDxsUJEp_oU8dWRPTj2Dsbe6c=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNM7CBlg4srZdX8GENen_u1Mn-HvoYzKTpIeMtGXCN7Lq-tiaH6jyBGyBpg9FYRcSc5hwGkxRVcl_TV5XHvQJbFcIzJ99LpNYuzjE54ZNVMKEYhTS4=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMbysucKVJNTNOZ9YNGHYdgSyYTNUQ1-cNPcJ53spqreDMhQlk5wKtgTfDLcHf0gjbTyx0Tc8zsTyUExz-hYatxpLqrDyTiA2bIwfDAk3ZRH0iCkhc=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczPKFiyypKN9joG4zhlg1vie_OUh2Re2zzY1B_XAtSYj4Ot0X4V0hn6d-NIF1E84-qQeaHBLRvHH_pT6hdanK3hNwnpejgpHz-P0xTAasU0omE5oZY0=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNfAeKYnafg3McnkXWojdm_tlNMTpn7k_AUEQxH15fxcpUyxNtkfxtDoQ47YLceBLE4gsQUYi1eoZq_QfT2UVK8NuNumwMM7ZzMnb1Xx1c0Vh3iBGU=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczPtgh1tKLsXJPyO753VZO6G9R6rcLMeRnbh1HtOhVgCMQP6Jj6Z_4qquhNoHRbgqtE-c5kNiV08yr8d8O_eEopWNQNK6KMxVRSYyckklBZFcgYjnQ0=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczOZAw94MG0XfiUp9VzJAFHXeCEmQ4DCBjrI5HrCDnoPLkVsoErHMTHFTcKMWkkaFVQMSSUknnZPDDKYhgOxXrn_e56JEHH4fLgCCAFWgnVm70UC56c=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMP3B_jHjlTQ737FDm0qA-dS8bssuTjA1cVRdqvx0YS0gD0MUWNaee7GIX8-fhAC1YkXGNZaWBEKHNvEJy681CB1CKcjikxgrHHrLIvCQoUFZ1LaZs=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczN1uInmbex-7itv3e6fxNDE6ea4DLONUpVw5hpBJvUnenSKIlgWss2UbWHMLtlarKViLN7K8p9V8CYSIjJvt7PH3usrV6a5_6k-zOcvkfHrFKo1roc=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNY-cd1t01xnlMaJ7xmKZfg6FCITz6WmsZ7WHbCKdD6YHOuGKKvoj4WgNbbKq7NvIHud_-Tk75kj2TfdErWUBhbWoETbQWpK9ELB3FN0aAIu83uWCw=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczMj6jAUFF6c5eUiPTVpq2xOTC-YrzhzY4cfXZpW3G8XkYzB90-nzAQV1JVgk2Ft7QyqLMXfc2ontQIsaam9ineTPFMN-lwj6o1OaQzYZ_2BzbRXziE=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNXvdI0eux6rQZoV3wtNmzLKX-eWiMHK_u25TlqHE-yt1kEdk8XmIPLQZgygH1JIMI1RH3BYO3Je3wsyC_BFnkfuxPXFXukFk8c9hDeDXrBgNBrkD8=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczPWj6f0dN-cDSeTSrEna48wz6sCXDH0wbaH0vE_oBs8UoIrGu0PftWYhJcjyIO1RE6uYKnnSlGMHp-rwtaJFcd4np4DWQE2uyUniG-fQgxcAPep5f4=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczOLQRwJYe1nrGQhdV2ds9OIm7os8KyroaLsqhBxqN52QY_xJVZ67CRADYXd8KUOp-parhZn__ORF0ylkbuW6pYrphCTu9ISLPs4z1pceaL5aAG_exM=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczO1F0kp7uAJM8nZWdV6uHDUrS4VAf5n4KvkvbmrWMSxxHsNGqlecbv4bM8h4rnnJhkabRdHbUZLLBCkUZxRzEGQXitqWFjsYDBJw4uaS59-bqmB9jQ=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczN7sAybk1ycqcUIQbB1SqLbXg1ocO3T6nrWEdywpgEbCK-X6J2QdBktrR8CObPvUemJO6UZu3y7vA3coXi6-2fnOEt6Vo53mao_QOeyUonBr9bIR7Q=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczPUpm7-cZFwhhA43SBXEiUw2pEIG0GTcgjTLcQiG4Z6faPLvb6-6OMYrTsUoK7JCGqKq6943aTclIjEQDYdq9Ig4abdygawjJjoF8zgt5gj71cAU3o=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczPIMx-Hd3q0tkfiaj9wDzEvRwMOy0OMAqS_pnxnSsS4-M1un1DJf5rZNkNm7YmMqEgwJbcDIX63ZpndzIqltNaTqq8t18S01r7OAX7xj-N6fEtoIvY=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczN_09PjSVXGkq68HXoRaIPiuqz8OLNsbp1YhidLz8E768aBBkl9B-VwLkWH7lJVIyOSbfAYHE0YUZIdJtX1LL156boDVPmY_cMo1xFAqqSyrnspL2I=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczOYfAUv1GHoxzPb1T-qBrT_rv-tXA-5A9Q0LyRYh_eo-Ww8kTDF9NkGg-frjdO_3RtpFbw1pPEthaX_W0mf1qLJlFt2ecTNUQSlkhpYDWjRrpuzpeg=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNFTdKxmTAamzQ0qTpt3nFfAn9jRFyBb_BDKiy6F9MCpN-lkyP4k6x2ou3Bgwa0zK4BrZU29SMcs6c6fcVzy97zgi_z_BlgbF0FowFaPAyZr8TkVjs=w1920-h1080"></object>
+                    <object data="https://lh3.googleusercontent.com/pw/AP1GczNpnQxn9lHGbaUsIC4-HpuxDp6lNLuPuiVOweejbjJY96038vxTat0_E_rH_S2522Tdisg1-xc4mvxhJn7KSQvORLXgJWMjx9S9TdpKqvO9QQ6wvdM=w1920-h1080"></object>
+                  </div>
+
+
+
+
+        </div>   
+    </div>        
+</div>
+</body>
+</html> 
